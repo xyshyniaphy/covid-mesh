8883ea1c36f5e739ef1c92d334e3fa3f0e87700d
li.mingrui <li.mingrui@tci-cn.co.jp>
2021-10-13 16:43:51 +0900

    add my api

---
+++ src/case-resolvers.js
@@ -0,0 +1,26 @@
+const toNumber =require( "lodash");
+
+const resolvers = {
+    Country: {
+      deathRate : async (input, args, apis) => {
+          if(input.mostRecent &&input.mostRecent.deaths){
+              console.log("input",input.mostRecent)
+              const populationResult = await apis.Polulation.api.population({country:"China"})
+              const population = toNumber(populationResult.populationString)
+              const deathRate = input.mostRecent.deaths/population
+            //   console.log("deathRate",deathRate)
+              return deathRate
+          }
+        return 0.0
+      },
+      population: async (input, args, apis) => {
+        const result = await apis.Polulation.api.population({country:"China"})
+        return toNumber(result.populationString)
+      },
+
+
+    },
+  };
+  
+  module.exports = { resolvers };
+  
